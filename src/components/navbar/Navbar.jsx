3ac33688d9e2448dd8526b822cfa4f102cd01944
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-01-22 14:17:44 +0300

    fix: tag

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 {/* sidebar */}
         <div className='wrapper'>
             <span>Jlord Dev</span>
-            <div className='social'></a>
+            <div className='social'>
             <a href=""><img src="/facebook.png" alt="" /></a>
             <a href=""><img src="/instagram.png" alt="" /></a>
             <a href=""><img src="/youtube.png" alt="" /></a>
