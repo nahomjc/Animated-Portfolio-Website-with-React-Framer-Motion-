095c9f8bb7247c442b66fdb5e194f43c5a3328c2
Nahom Tesfaye <82502560+nahomjc@users.noreply.github.com>
2024-02-01 01:55:54 -0800

    Update Test.jsx

--- src/Test.jsx
+++ src/Test.jsx
@@ -4,7 +4,7 @@ import {motion} from 'framer-motion'
 const Test = () => {
   return (
     <div className='course'>
-        <motion.div className='box' animate={{opacity:0}} transition={{duration:2}}></motion.div>
+        <motion.div className='box' animate={{opacity:0}} transition={{duration:3}}></motion.div>
     </div>
   )
 }
