e9ba31b25e404b2e6b15cd7e41da831b8674752a
nahomjc <aslandjc7@gmail.com>
2024-02-03 08:46:02 -0800

    feat: span framer style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import "./navbar.scss"
+import {motion} from 'framer-motion'
 const Navbar = () => {
   return (
     <div className='navbar'>
 {/* sidebar */}
         <div className='wrapper'>
-            <span>Jlord Dev</span>
+             <motion.span initial={{opacity:0 ,scale:0.5}}
+             animate={{opacity:1, scale:1}}
+             transition={{duration:0.5}}
+             >Jlord Dev</motion.span>
             <div className='social'>
             <a href=""><img src="/facebook.png" alt="" /></a>
             <a href=""><img src="/instagram.png" alt="" /></a>
