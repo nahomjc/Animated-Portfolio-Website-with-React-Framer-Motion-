4d801a033707d2149bfdccdb0455e5fcf407599e
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-06 15:41:39 +0300

    feat: mapping through links with id

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import '../sidebar.scss'
 const Links = () => {
+  const items = ['HomePage','Services','Portfolio','Contact','About']
   return (
-    <div>
-      
+    <div className='links'>
+      {items.map((item) =>(<a href={`#${item}`} key={item}>
+        {item}
+      </a>))}
     </div>
   );
 }
