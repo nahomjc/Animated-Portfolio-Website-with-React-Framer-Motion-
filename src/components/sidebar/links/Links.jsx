dff8f2f6e78d1f78c95dcabe1500459b04d47e48
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-06 17:06:18 +0300

    feat: motion style

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import '../sidebar.scss'
+import {motion} from 'framer-motion'
+const variants={
+  open:{
+    transition:{
+      staggerChildren:0.1
+    },
+  },
+  closed:{
+    transition:{
+      staggerChildren:0.05,
+      staggerDirection:-1,
+    }
+  }
+}
+const itemVariants={
+  open:{
+    y:0,
+    opacity:1
+  },
+  closed:{
+    y:50,
+    opacity:0
+  }
+}
 const Links = () => {
   const items = ['HomePage','Services','Portfolio','Contact','About']
   return (
-    <div className='links'>
-      {items.map((item) =>(<a href={`#${item}`} key={item}>
+    <motion.div className='links' variants={variants}>
+      {items.map((item) =>(<motion.a href={`#${item}`} key={item} variants={itemVariants}>
         {item}
-      </a>))}
-    </div>
+      </motion.a>))}
+    </motion.div>
   );
 }
 
