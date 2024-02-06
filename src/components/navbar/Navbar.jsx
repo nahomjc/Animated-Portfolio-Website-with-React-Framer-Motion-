cd9c34311cc333390b7a5d9a7e257f5e54101bfe
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-06 17:02:06 +0300

    feat:add sidebare

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+import Sidebar from "../sidebar/Sidebar"
 import "./navbar.scss"
 import {motion} from 'framer-motion'
 const Navbar = () => {
   return (
     <div className='navbar'>
-{/* sidebar */}
+<Sidebar/>
         <div className='wrapper'>
              <motion.span initial={{opacity:0 ,scale:0.5}}
              animate={{opacity:1, scale:1}}
