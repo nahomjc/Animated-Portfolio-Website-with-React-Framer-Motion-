de096bf8be1597263de98a11ad2f73e01177bbb9
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-09 17:23:41 +0300

    featL change vite port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port:  8080 // replace  8080 with any available port
+  }
 })
