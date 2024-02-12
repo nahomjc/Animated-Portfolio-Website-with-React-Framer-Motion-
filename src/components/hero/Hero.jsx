ec8986cdf1e2df11a3694542c46d64b2d0496b5e
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-12 12:05:55 +0300

    feat: hero style updated

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,12 +3,23 @@ import './hero.scss'
 const Hero = () => {
   return (
     <div className='hero'>
+        <div className="wrapper">
+        <div className="textContainer">
+            <h2>Nahom Tesfaye</h2>
+            <h1>Web developer</h1>
+            <div className="buttons">
+                <button>See the Latest Works</button>
+                <button id='contact'>Contact Me</button>
+            </div>
+            <img src="/scroll.png" alt="" />
+             </div>
+        </div>
         <div className="imageContainer">
             <img src="/profile.png" alt="" />
         </div>
     </div>
     
   )
-}
+} 
 
 export default Hero 
