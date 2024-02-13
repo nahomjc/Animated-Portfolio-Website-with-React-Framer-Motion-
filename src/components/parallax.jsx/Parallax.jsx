ea283d37188a1d291a8027aee0e86545c9fabf5c
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-02-13 16:24:45 +0300

    feat; condation and background image added

--- src/components/parallax.jsx/Parallax.jsx
+++ src/components/parallax.jsx/Parallax.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './parallax.scss'
-const Parallax = () => {
+const Parallax = ({type}) => {
   return (
-    <div className='parallax'>
+    <div className='parallax' style={{background:type==='services'? 'linear-gradient(180deg,#111132,#0c0c14)':'linear-gradient(180deg,#111132,#505064)'}}>
         <h1>{type==='services'? 'What We Do?':'What we Did'}</h1>
         <div className='mountains'></div>
         <div className='planets'></div>
