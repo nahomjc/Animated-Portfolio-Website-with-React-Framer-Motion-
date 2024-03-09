256a32bc28cbf7cade2c68905d8d55287095cba6
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-03-10 02:30:59 +0300

    feat: planets added

--- src/components/parallax.jsx/Parallax.jsx
+++ src/components/parallax.jsx/Parallax.jsx
@@ -1,24 +1,52 @@
-import React, { useRef } from 'react'
-import './parallax.scss'
-import {motion, useScroll, useTransform} from 'framer-motion'
-const Parallax = ({type}) => {
-  const ref =useRef()
-  const {scrollYProgress}=useScroll({
-    target:ref,
-    offset:['start start','end start']
+import { useRef } from "react";
+import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-  })
-  const yBg=useTransform(scrollYProgress,[0,1], ["0%", "100%"])
-  const yText=useTransform(scrollYProgress,[0,1], ["0%", "500%"])
+const Parallax = ({ type }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
+
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
-    <div className='parallax' style={{background:type==='Services'? 'linear-gradient(180deg,#111132,#0c0c14)':'linear-gradient(180deg,#111132,#505064)'}}>
-        <motion.h1 style={{y:yText}}>{type ==='Services'? 'What We Do?':'What we Did'}</motion.h1>
-        <motion.div  className='mountains'></motion.div>
-        <motion.div className='planets' style={{y:yBg, backgroundImage: `url(${type==='Services'? '/planets.png':"/sun.png"})`}}></motion.div>
-
-        <motion.div style={{x:yBg}}  className='stars'></motion.div>
+    <div
+      className="parallax"
+      ref={ref}
+      style={{
+        background:
+          type === "services"
+            ? "linear-gradient(180deg, #111132, #0c0c1d)"
+            : "linear-gradient(180deg, #111132, #505064)",
+      }}
+    >
+      <motion.h1 style={{ y: yText }}>
+        {type === "services" ? "What We Do?" : "What We Did?"}
+      </motion.h1>
+      <motion.div style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "/planet-icon.png" : "/planet3.png"
+          })`,
+        }} className="planet"></motion.div>
+      <motion.div className="mountains"></motion.div>
+     
+      <motion.div
+        className="planets"
+        style={{
+          y: yBg,
+          backgroundImage: `url(${
+            type === "services" ? "/planets.png" : "/sun.png"
+          })`,
+        }}
+      ></motion.div>
+      <motion.div style={{ x: yBg }} className="stars"></motion.div>
     </div>
-  )
-}
+  );
+};
 
-export default Parallax  
+export default Parallax;
