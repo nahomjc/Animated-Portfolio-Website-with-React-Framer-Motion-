794532d70ca0b29f242c47b8747f80d63dd824d2
nahomjc <82502560+nahomjc@users.noreply.github.com>
2024-07-04 14:57:19 +0300

    feat: add animation to services components

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,14 +1,36 @@
 import './services.scss'
 import {motion} from 'framer-motion'
+
+const variants ={
+    initial:{
+        x:-500,
+        y:100,
+        opacity:0,
+    },
+    animate:{
+        x:0,
+        opacity:1,
+        y:0,
+        transition:{
+            duration:1,
+            staggerChildren:0.1
+        },
+
+    }
+}
 const Services = () => {
   return (
-    <motion.div className='services'>
-        <motion.div className="textContainer">
+    <motion.div className='services'
+    variants={variants}
+    animate='animate'
+    initial='initial'
+    whileInView='animate '>
+        <motion.div className="textContainer" variants={variants}>
             <p>I focus on helping your bran grow and <br/>
              move forward.</p>
             <hr/>
         </motion.div>
-        <motion.div className="titleContainer">
+        <motion.div className="titleContainer" variants={variants}>
             <div className="title">
                 <img src='/people.webp' alt=''/>
                 <h1><b>Unique</b> Ideas</h1>
@@ -19,7 +41,7 @@ const Services = () => {
                 <button>WHAT WE Do</button>
             </div>
         </motion.div>
-        <motion.div className="listContainer">
+        <motion.div className="listContainer" variants={variants}>
             <motion.div className="box" whileHover={{background:'lightgray',color:'black'}}>
                 <h2>Branding</h2>
                 <p>orem ipsum dolor sit amet consecture adipiscing elit. 
